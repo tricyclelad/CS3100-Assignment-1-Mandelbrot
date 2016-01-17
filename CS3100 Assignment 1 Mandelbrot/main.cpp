e442ecbe8e27dd942f2e613113622398c4782156
Camron Blake Martinez <camron.martinez@aggiemail.usu.edu>
2016-01-16 18:22:47 -0700

    Initial Commit

---
+++ CS3100 Assignment 1 Mandelbrot/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS3100 Assignment 1 Mandelbrot
+//
+//  Created by Camron Blake Martinez on 1/16/16.
+//  Copyright © 2016 Camron Blake Martinez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
